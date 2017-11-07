1992631cdad07b27f39bbc215b1883a738e2233e
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-07 14:30:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,23 @@ function scuberGreetingForFeet (feet) {
     return "No can do.";
   }
 }
+
+function ternaryCheckCity (city) {
+  if (city = 'NYC') {
+    return 'Ok, sounds good.';
+  }
+  
+}
+
+
+
+
+describe('ternaryCheckCity()', function () {
+  it('returns "Ok, sounds good." when the city is NYC', function () {
+    expect(ternaryCheckCity('NYC')).to.equal('Ok, sounds good.');
+  });
+
+  it('should return "No go." if the destination city is not NYC', function () {
+    expect(ternaryCheckCity('Pittsburgh')).to.equal('No go.');
+  });
+});
