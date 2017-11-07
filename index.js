9778bef7b7b4f27bd2a1d5bf1a260f0547b48977
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-07 14:21:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 // Write your code in this file!
-describe('index.js', function () {
-  describe('scuberGreetingForFeet()', function () {
-    it('gives customers a free sample if the ride is less than or equal to 400 feet', function () {
-      expect(scuberGreetingForFeet(199)).to.equal('This one is on me!');
-    });
+
 
     function scuberGreetingForFeet (feet) {
       if (feet <= 400) {
