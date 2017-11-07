054eea5437077c55131ea5989c1aaeb4dcfdc9b0
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-07 14:23:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // Write your code in this file!
 
 
-    function scuberGreetingForFeet () {
-      let feet;
-      if (feet <= 400) {
-        feet = 'This one is on me!';
-      }
+function scuberGreetingForFeet() {
+let feet;
+if (feet <= 400) {
+feet = 'This one is on me!';
+} else if (1999 < feet && feet < 2500) {
+feet = "I will gladly take your thirty bucks.";
+} else {
+feet = "No can do.";
+}
+return feet
+}
