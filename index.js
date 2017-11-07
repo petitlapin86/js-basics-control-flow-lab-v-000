a303e89446c680464ae8538602e904d2e21e201f
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-07 14:46:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,13 +15,29 @@ function ternaryCheckCity (city) {
   return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
+switch switchOnCharmFromTip (tip) {
+  generous = "Thank you so much."
+  break;
+  not as generous = 'Thank you.'
+  break;
+  thanks for everything = 'Bye.'
+}
 
-describe('ternaryCheckCity()', function () {
-  it('returns "Ok, sounds good." when the city is NYC', function () {
-    expect(ternaryCheckCity('NYC')).to.equal('Ok, sounds good.');
+
+
+describe('switchOnCharmFromTip()', function () {
+  it('should return "Thank you so much." if the tip is generous', function () {
+    expect(switchOnCharmFromTip('generous')).to.equal('Thank you so much.');
   });
 
-  it('should return "No go." if the destination city is not NYC', function () {
-    expect(ternaryCheckCity('Pittsburgh')).to.equal('No go.');
+  it('should return "Thank you." if the tip is not as generous', function () {
+    expect(switchOnCharmFromTip('not as generous')).to.equal('Thank you.');
+  });
+
+  it('should return "Bye." if anything else', function () {
+    expect(switchOnCharmFromTip('thanks for everything')).to.equal('Bye.');
   });
 });
+});
+
+
